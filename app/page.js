d43fbd8682e34b5755261709cf209234b0219e94
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2023-09-21 16:35:33 -0600

    Week 3 - No Style

--- app/page.js
+++ app/page.js
@@ -7,7 +7,8 @@ export default function Home() {
             <h1>CPRG 306: Web Development 2 - Assignments</h1>
             <StudentInfo />
             <br/>
-            <Link href="week2">Week 2</Link>
+            <Link href="week2">Week 2</Link><br/>
+            <Link href="week3">Week 3</Link>
         </>
     );
 }
