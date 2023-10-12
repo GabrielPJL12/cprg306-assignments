28b70e40ed68d6388c0faaab60817d4e3ad891ed
Gabriel Leclerc <gleclerc.yk1@gmail.com>
2023-10-12 15:27:37 -0600

    divs

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -20,27 +20,43 @@ export default function NewItem() {
 
     return (
         <main>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name: </label>
-                <input id="name" type="text" value={name} onChange={(event) => setName(event.target.value)} />
-                <br /><br />
-                <label htmlFor="quantity">Quantity: </label>
-                <input id="quantity" type="number" value={quantity} onChange={(event) => setQuantity(event.target.value)} />
-                <br /><br />
-                <label htmlFor="category">Category: </label>
-                <select id="category" value={category} onChange={(event) => setCategory(event.target.value)}>
-                    <option value="Produce">Produce</option>
-                    <option value="Dairy">Dairy</option>
-                    <option value="Bakery">Bakery</option>
-                    <option value="Meat">Meat</option>
-                    <option value="Frozen">Frozen Foods</option>
-                    <option value="Canned">Canned Goods</option>
-                    <option value="Beverages">Beverages</option>
-                    <option value="Snacks">Snacks</option>
-                    <option value="Other">Other</option>
-                </select>
-                <br /><br />
-                <button type="submit">Add Item</button>
+            <form onSubmit={handleSubmit} className="flex justify-center">
+                <div >
+                    <div>
+                        <label htmlFor="name">Name: </label>
+                        <input id="name" type="text" value={name} onChange={(event) => setName(event.target.value)} />
+                    </div>
+                </div>
+                <br />
+                <div>
+                    <div>
+                        <label htmlFor="quantity">Quantity: </label>
+                        <input id="quantity" type="number" value={quantity} onChange={(event) => setQuantity(event.target.value)} />
+                    </div>
+                </div>
+                <br />
+                <div>
+                    <div>
+                        <label htmlFor="category">Category: </label>
+                        <select id="category" value={category} onChange={(event) => setCategory(event.target.value)}>
+                            <option value="Produce">Produce</option>
+                            <option value="Dairy">Dairy</option>
+                            <option value="Bakery">Bakery</option>
+                            <option value="Meat">Meat</option>
+                            <option value="Frozen">Frozen Foods</option>
+                            <option value="Canned">Canned Goods</option>
+                            <option value="Beverages">Beverages</option>
+                            <option value="Snacks">Snacks</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                </div>
+                <br />
+                <div>
+                    <div>
+                        <button type="submit">Add Item</button>
+                    </div>
+                </div>
             </form>
         </main>
     )
