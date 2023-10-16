0c97beafcafb0d15551e302ba368cfc8f4ccf5b2
GabrielPJL12 <gleclerc.yk1@gmail.com>
2023-10-16 17:19:20 -0600

    sort buttons

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -16,11 +16,28 @@ export default function ItemList() {
         }
     }
 
+    function SortButtons() {
+        if (sortBy === "name") {
+            return (
+                <div className="flex justify-center space-x-4">
+                    <button onClick={handleSort} value="name" class="bg-slate-400 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black ring-1 ring-inset ring-gray-500/10">Sort by Name</button> 
+                    <button onClick={handleSort} value="category" class="bg-slate-200 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black ring-1 ring-inset ring-gray-500/10">Sort by Category</button>
+                </div>
+            )
+        } else if (sortBy === "category") { 
+            return (
+                <div className="flex justify-center space-x-4">
+                    <button onClick={handleSort} value="name" class="bg-slate-200 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black ring-1 ring-inset ring-gray-500/10">Sort by Name</button> 
+                    <button onClick={handleSort} value="category" class="bg-slate-400 inline-flex items-center rounded-md px-2 py-1 text-xs font-medium text-black ring-1 ring-inset ring-gray-500/10">Sort by Category</button>
+                </div>
+            )
+        }
+    }
+
     return (
         <main>
-            <button onClick={handleSort} value="name">Sort by Name</button> <br/>
-            <button onClick={handleSort} value="category">Sort by Category</button>
             <h2 className="text-2xl font-bold">Items</h2>
+            <SortButtons />
             <div>
                 {items.map((item) => (
                     <Item key={item.id} {...item} />
