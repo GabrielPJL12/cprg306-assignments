43b9f4c5b5dee2ba6fd334a5eb5cb63d0f5e037b
GabrielPJL12 <gleclerc.yk1@gmail.com>
2023-10-16 16:37:39 -0600

    item-list

--- app/week5/item-list.js
+++ app/week5/item-list.js
@@ -5,8 +5,21 @@ import Item from "./item";
 import items from "./items";
 
 export default function ItemList() {
+    const [sortBy, setSortBy] = useState("name");
+
+    const handleSort = (event) => {
+        setSortBy(event.target.value);
+        if (event.target.value === "name") {
+            items.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (event.target.value === "category") {
+            items.sort((a, b) => a.category.localeCompare(b.category));
+        }
+    }
+
     return (
         <main>
+            <button onClick={handleSort} value="name">Sort by Name</button> <br/>
+            <button onClick={handleSort} value="category">Sort by Category</button>
             <h2 className="text-2xl font-bold">Items</h2>
             <div>
                 {items.map((item) => (
@@ -16,3 +29,4 @@ export default function ItemList() {
         </main>
     )
 }
+
